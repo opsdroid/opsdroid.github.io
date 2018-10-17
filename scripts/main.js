8caad8bb2ccd0a5763917c8db18f4b4f876ac231
FabioRosado <fabioglrosado@gmail.com>
2018-10-17 11:43:02 +0100

    remove commented out line

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,7 @@ const navigation = document.getElementsByClassName('navigation');
 
 collapseBtn.addEventListener('click', function(e){
   collapseBtn.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
-  // navigation[0].setAttribute('id', navigation[0].getAttribute('id') === 'collapse' ? '' : 'collapse');
   navigation[0].setAttribute('style', navigation[0].getAttribute('style') === 'display: block;' ? '' : 'display: block;');
-
-  
 });
 
 ////////////////////////////////////////////////////////////////////////
